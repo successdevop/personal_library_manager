6e68c465f0b667ed2638f2b838c4deb53def22ee
successdevop <successraphael28@gmail.com>
2026-03-25 11:02:12 +0100

    implemented the display all books function, that allows the user to see all books in their library

--- services.py
+++ services.py
@@ -1,8 +1,10 @@
 import random
+from operator import itemgetter
 
 library = [
     {"title": "The Hobbit", "author": "J.R.R. Tolkien", "year": 1937, "genre": "Fantasy", "read": True, "rating": 5},
     {"title": "1984", "author": "George Orwell", "year": 1949, "genre": "Dystopian", "read": False, "rating": None},
+    {"title": "Dune", "author": "Frank Herbert", "year": 1965, "genre": "Science Fiction", "read": True, "rating": 4},
     {"title": "Dune", "author": "Frank Herbert", "year": 1965, "genre": "Science Fiction", "read": True, "rating": 4}
 ]
 
@@ -43,7 +45,7 @@ def read_book() -> bool:
     return answer[random.randint(0, 1)]
 
 
-def book_rating(val):
+def book_rating(val: bool):
     """
     this function generates random numbers between, 1 to 10
     if the number is between, 1 to 5, it returns the number
@@ -56,7 +58,7 @@ def book_rating(val):
     return None
 
 
-def add_book(data):
+def add_book(data: list):
     """This function adds a new book to the library"""
 
     title = validate_string("Enter the title: ")
@@ -72,7 +74,7 @@ def add_book(data):
     print(data)
 
 
-def remove_book(data):
+def remove_book(data: list):
     """this function removes a book from the libreary by the book's title"""
     book_r = input("Enter the title of the book: ")
     for book in data:
@@ -82,5 +84,50 @@ def remove_book(data):
             return
     print("Book not found")
 
+
+def display_all_books(data: list):
+    """ this function displays all books in our library"""
+    for book in data:
+        print(f"Book: {book['title']} | Author: {book['author']}")
+
+
+def view_statistics(data: list):
+    """ this function shows the different statistics about books in our library"""
+    total_books = len(data)
+    total_read_books = 0
+    total_unread_books = 0
+    average_rating_of_read_books = 0
+    most_common_genre_dict = {}
+    most_common_genre = ""
+    common_genre = 0
+
+    print(data)
+    new_data = sorted(data, key=itemgetter("year"))
+    print(new_data)
+    books_per_decade = 0
+
+    for stat in data:
+        if not stat["read"]:
+            total_unread_books += 1
+        if stat["rating"]:
+            average_rating_of_read_books += stat["rating"]
+        most_common_genre_dict[stat["genre"]] = most_common_genre_dict.setdefault(stat["genre"], 0) + 1
+
+    print(f"total books: {total_books}")
+    print(f"Unread book count: {total_unread_books}")
+    print(f"Average rating of read book: {average_rating_of_read_books / (total_books - total_unread_books)}")
+    for k, v in most_common_genre_dict.items():
+        if common_genre < v:
+            most_common_genre = k
+        else:
+            most_common_genre = None
+    print(f"Most common genre: {most_common_genre}")
+
+
+
+
+
 # add_book(library)
 # remove_book(library)
+# display_all_books(library)
+view_statistics(library)
